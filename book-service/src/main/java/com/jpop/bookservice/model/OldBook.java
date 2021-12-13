1d035fdd45fe9d8482101c40649602e3e5b110a6
sskadam45 <once4sk@gmail.com>
2021-12-13 13:53:09 +0530

    Book service:
    -resolved Review comments
    -Added change for other servicess like eureka,rabittmq,configserver etc

---
+++ book-service/src/main/java/com/jpop/bookservice/model/OldBook.java
@@ -0,0 +1,53 @@
+package com.jpop.bookservice.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "oldbooks")
+public class OldBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "price")
+    private int price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
--- book-service/src/main/java/com/jpop/bookservice/service/BookService.java
+++ book-service/src/main/java/com/jpop/bookservice/service/BookService.java
@@ -1,7 +1,9 @@
 package com.jpop.bookservice.service;
 
 import com.jpop.bookservice.model.Book;
+import com.jpop.bookservice.model.OldBook;
 import com.jpop.bookservice.repository.BookRepository;
+import com.jpop.bookservice.repository.OldBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +15,22 @@ public class BookService {
     @Autowired
     BookRepository bookRepository;
 
+    @Autowired
+    OldBookRepository oldBookRepository;
+
     public List<Book> findAll() {
         return bookRepository.findAll();
     }
-
+    public List<OldBook> findAllOldBooks() {
+        return oldBookRepository.findAll();
+    }
     public Optional<Book> findById(Long id) {
         return bookRepository.findById(id);
     }
 
+    public OldBook save(OldBook book) {
+        return oldBookRepository.save(book);
+    }
     public Book save(Book book) {
         return bookRepository.save(book);
     }
